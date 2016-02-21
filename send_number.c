424fccfc4ed9a9f317446886db8e70448d130691
Suhas Bhairav <suhasbhairav@gmail.com>
2016-02-21 23:38:52 +0100

    Basic send program

---
+++ send_number.c
@@ -0,0 +1,25 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	MPI_Init(NULL, NULL);
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	int number;
+	if( world_rank == 0){
+		number = 100;
+		MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		printf("Sent number: %d\n", number);
+	}else if(world_rank == 1){
+
+		MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("Received number:%d\n", number);
+	}
+
+
+	MPI_Finalize();
+}
